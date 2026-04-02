92b578bc9886d7e596390389530428f590dd9a1a
ratheechakshita418 <ratheechakshita418@gmail.com>
2026-04-02 10:33:44 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h2 className="text-center">Shopping Cart</h2>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
